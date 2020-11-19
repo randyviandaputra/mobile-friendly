06b2935d562ab8c990ceac2a076408bcedd6a072
Randy Vianda Putra <modulloe@gmail.com>
2020-11-19 17:46:26 +0700

    init: commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Home, Bookmark, User } from "react-feather";
+import {
+  Styles,
+  CSSReset,
+  MobileNavbar,
+  Navbar
+} from './AppStyled'
 
 function App() {
+  const [windowDimension, setWindowDimension] = useState(null);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 640;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Styles.Wrapper>
+      <CSSReset />
+
+      {isMobile ? (
+        <MobileNavbar.Wrapper>
+          <MobileNavbar.Items>
+            <MobileNavbar.Item>
+              <MobileNavbar.Icon>
+                <Home size={16} />
+              </MobileNavbar.Icon>
+              Home
+            </MobileNavbar.Item>
+            <MobileNavbar.Item>
+              <MobileNavbar.Icon>
+                <Bookmark size={16} />
+              </MobileNavbar.Icon>
+              Blog
+            </MobileNavbar.Item>
+            <MobileNavbar.Item>
+              <MobileNavbar.Icon>
+                <User size={16} />
+              </MobileNavbar.Icon>
+              About
+            </MobileNavbar.Item>
+          </MobileNavbar.Items>
+        </MobileNavbar.Wrapper>
+      ) : (
+        <Navbar.Wrapper>
+          <Navbar.Logo>Logo</Navbar.Logo>
+          <Navbar.Items>
+            <Navbar.Item>Home</Navbar.Item>
+            <Navbar.Item>Blog</Navbar.Item>
+            <Navbar.Item>About</Navbar.Item>
+          </Navbar.Items>
+        </Navbar.Wrapper>
+      )}
+    </Styles.Wrapper>
   );
 }
 
---
+++ src/AppStyled.js
@@ -0,0 +1,81 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const Styles = {
+  Wrapper: styled.main`
+    display: flex;
+    background-color: #eeeeee;
+    height: 100vh;
+  `
+};
+
+export const Navbar = {
+  Wrapper: styled.nav`
+    flex: 1;
+
+    align-self: flex-start;
+
+    padding: 1rem 3rem;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    background-color: #09f;
+  `,
+  Logo: styled.h1`
+    border: 1px solid gray;
+    padding: 0.5rem 1rem;
+  `,
+  Items: styled.ul`
+    display: flex;
+    list-style: none;
+  `,
+  Item: styled.li`
+    padding: 0 1rem;
+    cursor: pointer;
+  `
+};
+
+export const MobileNavbar = {
+  Wrapper: styled(Navbar.Wrapper)`
+    position: fixed;
+    width: 100vw;
+    bottom: 0;
+
+    justify-content: center;
+  `,
+  Items: styled(Navbar.Items)`
+    flex: 1;
+    padding: 0 2rem;
+
+    justify-content: space-around;
+  `,
+  Item: styled(Navbar.Item)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    font-size: 1.2rem;
+  `,
+  Icon: styled.span``
+};
+
+export const CSSReset = createGlobalStyle`
+  *,
+  *::before, 
+  *::after {
+    margin: 0; 
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    font-size: 62.5%; /*1rem = 10px*/
+    box-sizing: border-box;      
+  }  
+
+  body {
+    font-size: 1.4rem;
+    font-family: sans-serif;
+  }
+`;
